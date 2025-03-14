b0dad40b098cd63b39cb290e44a0ce27ad872f8f
jmpark5110 <jmpark@nerdystar.io>
2025-03-14 18:03:32 +0900

    chore: prettier with rush setup

--- shared-schema/base/src/user/user.schema.ts
+++ shared-schema/base/src/user/user.schema.ts
@@ -1,20 +1,23 @@
-import { z } from "zod";
-import { baseSchema } from "@BE-common/base/schema";
+import { z } from 'zod';
+import { baseSchema } from '@BE-common/base/schema';
 
+// it can be both user domain and user persistence schema
+// usually, it is used for just user domain schema only
+// if you want to use it for user persistence schema, you should extend it
 export const commonUserSchema = baseSchema.extend({
   firstName: z.string(),
   lastName: z.string(),
   age: z.number(),
-  email: z.string().email().optional(),
+  email: z.string().email().optional()
 });
 
 // user domain schema
 export const userSchema = commonUserSchema.extend({
-  fullName: z.string(),
+  fullName: z.string()
 });
 
 // user persistence schema
 export const userPersistenceSchema = commonUserSchema.extend({
   version: z.number().optional(),
-  createdAt: z.date(),
+  createdAt: z.date()
 });
